3251d556221ca2ac92404a227c6728036698f78f
Hieu Nguyen <hieunv2@bssd.vn>
2024-09-20 16:33:48 +0700

    update

--- app/_api/collection.ts
+++ app/_api/collection.ts
@@ -3,7 +3,7 @@ import { API_PATH, CARD_WRAPPER, COLLECTORS } from './constant';
 import { FetchServerResponseResult } from 'next/dist/client/components/router-reducer/fetch-server-response';
 
 function getCollectors() {
-  return fetch(API_PATH + COLLECTORS, { cache: 'no-store' })
+  return fetch(API_PATH + COLLECTORS)
     .then((response: any) => response.json())
     .catch((error: any) => console.log(error));
 }
--- app/_api/emblems.ts
+++ app/_api/emblems.ts
@@ -3,13 +3,13 @@ import { API_PATH, CARD_WRAPPER, COLLECTORS, EMBLEMS } from './constant';
 import { FetchServerResponseResult } from 'next/dist/client/components/router-reducer/fetch-server-response';
 
 function getEmblems() {
-  return fetch(API_PATH + EMBLEMS, { cache: 'no-store' })
+  return fetch(API_PATH + EMBLEMS)
     .then((response: any) => response.json())
     .catch((error: any) => console.log(error));
 }
 
 function getEmblemDetail(emblemId: string) {
-  return fetch(API_PATH + EMBLEMS + emblemId, { cache: 'no-store' })
+  return fetch(API_PATH + EMBLEMS + emblemId)
     .then((response: any) => response.json())
     .catch((error: any) => console.log(error));
 }
